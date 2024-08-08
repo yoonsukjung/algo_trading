1329687e951e8fb1b208e681833309988f4b08cc
Yoonsuk Jung <yoonsukjung@Yoonsukui-MacBookPro.local>
2024-08-09 03:55:06 +0900

    Remove directory from tracking

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-data_path = '/Users/yoonsukjung/Desktop/data/15m_2024-07-12'
-result_path_strategy1 = '/Users/yoonsukjung/PycharmProjects/algo_trading/15m_results/strategy1_result'
-result_path_strategy2 = '/Users/yoonsukjung/PycharmProjects/algo_trading/15m_results/strategy2_result'
-
